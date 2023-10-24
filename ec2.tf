a5cbed0de0626b91061c4e7d8683c85e8d2d417a
Mohnish Ramanujam <>
2023-10-24 23:23:18 +1300

    fix ec2 instance

--- ec2.tf
+++ ec2.tf
@@ -1,18 +1,62 @@
-// Append to ec2.tf
+resource "aws_launch_configuration" "ecs" {
+  name_prefix                 = "img-upscaler"
+  image_id                    = "ami-0efcece6bed30fd98"
+  instance_type               = "g4dn.xlarge"
+  iam_instance_profile        = aws_iam_instance_profile.ecs_instance_profile.name
 
-resource "aws_instance" "upscaler_instance" {
-  ami             = "ami-0efcece6bed30fd98"  // Replace with ECS-optimized AMI ID
-  instance_type   = "g4dn.xlarge"
-  subnet_id       = aws_subnet.upscaler_subnet.id
-  vpc_security_group_ids = [aws_security_group.upscaler_sg.id]
-  iam_instance_profile = aws_iam_instance_profile.upscaler_instance_profile.name
-
+  # Ensure the EC2 instance registers with the ECS Cluster
   user_data = <<-EOF
               #!/bin/bash
-              echo ECS_CLUSTER=image_upscaler_cluster >> /etc/ecs/ecs.config
+              echo "ECS_CLUSTER=${aws_ecs_cluster.my_cluster.name}" >> /etc/ecs/ecs.config
               EOF
 
-  tags = {
-    Name = "ECS Instance - Image Upscaler"
+  lifecycle {
+    create_before_destroy = true
   }
 }
+
+
+resource "aws_autoscaling_group" "ecs" {
+  launch_configuration = aws_launch_configuration.ecs.name
+  desired_capacity     = 1
+  max_size             = 1
+  min_size             = 1
+
+  # Define which VPC and subnet to launch the instance into.
+  vpc_zone_identifier  = [aws_subnet.upscaler_subnet.id] # Change this to your subnet ID
+
+  lifecycle {
+    create_before_destroy = true
+  }
+}
+
+resource "aws_iam_instance_profile" "ecs_instance_profile" {
+  name = "ecs-instance-profile"
+  role = aws_iam_role.ecs_instance.name
+}
+
+resource "aws_iam_role" "ecs_instance" {
+  name = "ecs-instance-role"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17",
+    Statement = [
+      {
+        Action = "sts:AssumeRole",
+        Effect = "Allow",
+        Principal = {
+          Service = "ec2.amazonaws.com"
+        }
+      }
+    ]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "ecs_instance" {
+  role       = aws_iam_role.ecs_instance.name
+  policy_arn = "arn:aws:iam::aws:policy/service-role/AmazonEC2ContainerServiceforEC2Role"
+}
+
+
+
+
--- outputs.tf
+++
@@ -1,4 +0,0 @@
-output "ecs_service_url" {
-  description = "URL for the deployed ECS service"
-  value       = "http://${aws_instance.upscaler_instance.public_ip}"
-}
