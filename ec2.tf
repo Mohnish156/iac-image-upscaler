e6fe47de0f2ae1cef0896a08332ede8a49e1bcaa
Mohnish Ramanujam <>
2023-10-24 20:13:06 +1300

    base image

--- ec2.tf
+++ ec2.tf
@@ -1,7 +1,7 @@
 // Append to ec2.tf
 
 resource "aws_instance" "upscaler_instance" {
-  ami             = "ami-03f65b8614a860c29"  // Replace with ECS-optimized AMI ID
+  ami             = "ami-0efcece6bed30fd98"  // Replace with ECS-optimized AMI ID
   instance_type   = "g4dn.xlarge"
   subnet_id       = aws_subnet.upscaler_subnet.id
   vpc_security_group_ids = [aws_security_group.upscaler_sg.id]
@@ -9,7 +9,7 @@ resource "aws_instance" "upscaler_instance" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo ECS_CLUSTER=upscaler_cluster >> /etc/ecs/ecs.config
+              echo ECS_CLUSTER=image_upscaler_cluster >> /etc/ecs/ecs.config
               EOF
 
   tags = {
